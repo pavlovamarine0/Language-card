f94e4da4aef33f647af2c4630a164aace0d76e93
Maryna <pavlovamarine0@gmail.com>
2026-08-06 19:46:52 +0200

    init app

---
+++ LanguageCard/src/test/java/com/maryna/LanguageCard/LanguageCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maryna.LanguageCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
